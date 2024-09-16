48c75c6db88c873dcd3ea9acf85b4607081f7df8
Nicolaj Sylvester Thygesen <nst@norlysenergytrading.com>
2024-09-16 11:50:37 +0200

    Created attributes and ApiCallType.cs

---
+++ AttributeApiRepoFactory/Attributes/ApiRepositoryAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace AttributeApiRepoFactory.Attributes;
+
+[AttributeUsage(AttributeTargets.Interface)]
+public class ApiRepositoryAttribute : Attribute
+{
+    public string BaseUrl { get; }
+    public ApiRepositoryAttribute(string baseUrl)
+    {
+        BaseUrl = baseUrl;
+    }
+}
